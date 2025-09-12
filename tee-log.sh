05106d0a011aed1d56c3ac33a90ff44550446931
Ivan Marcelino <ivanmarcelino2004@gmail.com>
2025-09-11 21:54:12 -0300

    add tee-log

---
+++ tee-log.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+	echo "Uso: $0 ARQUIVO_LOG"
+	echo "Copia a entrada (stdin) para stdout e também grava em arquivo com timestamp."
+	echo "Exemplo: $0 logs/raw.log"
+	exit 1
+fi
+
+mapfile -t input
+
+timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+for lines in "${input[@]}"; do
+	echo -e "$timestamp\t$lines" >>$1
+done
